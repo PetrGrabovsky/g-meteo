da511e08466191790902206ddc5b2f94a10b6919
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-09-27 20:44:28 +0200

    chore: Aktualizace závislostí na nejnovější verze

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
-import { ReactNode } from "react";
-import "./globals.css";
-import { inter } from "@/utils/fonts";
-import clsx from "clsx";
+import { ReactNode } from 'react';
+import './globals.css';
+import { inter } from '@/utils/fonts';
+import clsx from 'clsx';
 
 interface RootLayoutProps {
-  children: ReactNode
+  children: ReactNode;
 }
 
-export default function RootLayout({children}: RootLayoutProps) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="cs">
       <body className={clsx(inter.className, 'antialiased')}>{children}</body>
     </html>
-  )
+  );
 }
