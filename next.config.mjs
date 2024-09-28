a6a4385e2c6b2e85c5ff48a03c46b19eb77331cd
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-09-28 11:13:12 +0200

    chore: Přidána konfigurace pro načítání vzdálených obrázků do next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'openweathermap.org',
+        port: '',
+        pathname: '/img/wn/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
