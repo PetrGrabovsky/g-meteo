7b3c1f70bd2071745ab2809860049d1aa3043a13
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-09-28 23:49:35 +0200

    refactor: Oddělení metadat z RootLayout do souboru utils/metadata.ts - Metadata importována a integrována do RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,36 +3,10 @@ import './globals.css';
 import { inter } from '@/utils/fonts';
 import clsx from 'clsx';
 import { Metadata } from 'next';
+import { metadata as myMetadata } from '@/utils/metadata';
 import LayoutContent from '@/components/layout-content';
 
-export const metadata: Metadata = {
-  title: 'Gmeteo - předpověď počasí na 6 dní pro celý svět',
-  description:
-    'S aplikací Gmeteo získáte přesnou a aktuální předpověď počasí na 6 dní pro jakékoliv místo na světě. Zadejte své město a zjistěte více o počasí.',
-  keywords: [
-    'počasí',
-    'předpověď',
-    '6denní předpověď',
-    'Gmeteo',
-    'předpověď počasí',
-    'světové počasí',
-    'počasí ve městě',
-    'meteorologie',
-  ],
-  authors: [
-    {
-      name: 'Petr Grabovský',
-      url: 'https://github.com/PetrGrabovsky',
-    },
-  ],
-  viewport: 'width=device-width, initial-scale=1',
-  openGraph: {
-    title: 'Gmeteo - Předpověď počasí na 6 dní pro celý svět',
-    description: 'Získejte přesnou předpověď počasí na 6 dní pro jakékoliv místo na světě s aplikací Gmeteo.',
-    url: 'https://g-meteo-eight.vercel.app/',
-    type: 'website',
-  },
-};
+export const metadata: Metadata = myMetadata;
 
 interface RootLayoutProps {
   children: ReactNode;
