f979c371765636740d51e6c0ef8eb3f8666a167f
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-09-28 21:07:05 +0200

    feat: Přidán thunk filterCity a pomocné funkce pro filtrování měst - Vytvořen thunk filterCity v souboru store/thunks/filter-cities.ts - Implementována debounced funkce debouncedFilterCities pro optimalizaci výkonu při filtrování

---
+++ store/thunks/filter-cities.ts
@@ -0,0 +1,33 @@
+import { AppDispatch, RootState } from '../store';
+import { setCitySuggestions } from '../slices/city-slice';
+import { debounce } from 'lodash';
+import { City } from '@/utils/weather-api-types';
+
+// Vrací města na základě vstupu
+const filterCitiesByInput = (allCities: City[], searchInput: string): City[] => {
+  const trimmedSearchInput = searchInput.trim().toLowerCase();
+
+  if (!trimmedSearchInput) return [];
+
+  return allCities.filter((city) => city.name.toLowerCase().startsWith(trimmedSearchInput)).slice(0, 5);
+};
+
+// Debounced funkce pro optimalizaci výkonu
+const debouncedFilterCities = debounce((dispatch: AppDispatch, getState: () => RootState) => {
+  const {
+    city: { allCities, searchInput },
+  } = getState();
+
+  const filteredCities = filterCitiesByInput(allCities, searchInput);
+
+  dispatch(setCitySuggestions(filteredCities));
+}, 500);
+
+// Thunk pro filtrování měst
+export const filterCities = () => (dispatch: AppDispatch, getState: () => RootState) => {
+  const { allCities } = getState().city;
+
+  if (!allCities.length) return;
+
+  debouncedFilterCities(dispatch, getState);
+};
