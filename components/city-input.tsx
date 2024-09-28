d67e0fb55cc3019990aaec5fccde17bcf329d136
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-09-28 21:13:04 +0200

    feat: Vytvořen našeptávač v komponentě CityInput - Vytvořen seznam pro zobrazení dynamických návrhů měst na základě uživatelského vstupu - Přidána funkce handleInputChange pro vyhledávání a filtrování měst v CityInput - Přidána funkce handleInputBlur pro resetování vyhledávacího pole a návrhů při ztrátě focusu

--- components/city-input.tsx
+++ components/city-input.tsx
@@ -1,12 +1,16 @@
 import { useEffect, useRef } from 'react';
 import { fetchAllCities } from '@/store/thunks/fetch-all-cities';
 import { useAppSelector, useAppDispatch } from '@/store/hooks';
+import { setSearchInput, setCitySuggestions } from '@/store/slices/city-slice';
+import { filterCities } from '@/store/thunks/filter-cities';
 import clsx from 'clsx';
 
 export default function CityInput() {
   const dispatch = useAppDispatch();
   const allCities = useAppSelector((state) => state.city.allCities);
   const loading = useAppSelector((state) => state.city.loading);
+  const searchInput = useAppSelector((state) => state.city.searchInput);
+  const citySuggestions = useAppSelector((state) => state.city.citySuggestions);
   const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
@@ -21,11 +25,24 @@ export default function CityInput() {
     }
   }, [loading]);
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setSearchInput(e.target.value));
+    dispatch(filterCities());
+  };
+
+  const handleInputBlur = () => {
+    dispatch(setSearchInput(''));
+    dispatch(setCitySuggestions([]));
+  };
+
   return (
     <div className="relative w-48">
       <input
         ref={inputRef}
         type="text"
+        value={searchInput}
+        onChange={handleInputChange}
+        onBlur={handleInputBlur}
         placeholder={loading ? 'Načítám města...' : 'Zadejte město...'}
         disabled={loading}
         maxLength={20}
@@ -36,6 +53,28 @@ export default function CityInput() {
           'text-center text-sm hover:scale-105 focus:scale-105 focus:ring-blue-400'
         )}
       />
+      {citySuggestions.length > 0 && (
+        <div
+          className={clsx(
+            'absolute z-10 mt-2 w-full overflow-hidden rounded-lg border border-gray-200 bg-white shadow-lg'
+          )}
+        >
+          <ul className="max-h-60">
+            {citySuggestions.map((city) => (
+              <li
+                key={city.id}
+                className={clsx(
+                  'cursor-pointer px-4 py-2 text-gray-700 transition-colors duration-200',
+                  'hover:bg-blue-500 hover:text-white'
+                )}
+                onClick={() => dispatch(setSearchInput(city.name))}
+              >
+                {city.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
