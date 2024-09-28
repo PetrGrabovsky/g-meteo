15e8e42ba506bc4291d2001ef1ee7772ce43885a
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-09-28 11:10:57 +0200

    style: Přidána definice animace fadeIn do tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,15 @@ const config: Config = {
           lg: '1024px',
         },
       },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 0.5s ease-in-out',
+      },
     },
   },
   plugins: [],
