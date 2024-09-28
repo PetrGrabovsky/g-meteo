690bbed633599938cd4ae37910a800b7358396cb
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-09-28 23:12:10 +0200

    feat: Přidání favicon, manifestu a souvisejících ikon do public - Vytvořeny základní metadata v RootLayout - Přidány relevantní tagy do head elementu v RootLayout

--- components/weather-card-list.tsx
+++ components/weather-card-list.tsx
@@ -20,7 +20,7 @@ export default function WeatherCardList() {
   };
 
   return (
-    <ul className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 lg:justify-center">
+    <ul className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 lg:justify-center">
       {dailyRecords.map((record) => (
         <li key={record.date}>
           <WeatherCard
