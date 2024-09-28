10317f1b857fc61810a016a610045ded59a994ed
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-09-28 23:54:15 +0200

    refactor: Oddělen element head z RootLayout do souboru components/head.tsx - Komponenta Head integrována do RootLayout pro vyšší přehlednost

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import clsx from 'clsx';
 import { Metadata } from 'next';
 import { metadata as myMetadata } from '@/utils/metadata';
 import LayoutContent from '@/components/layout-content';
+import Head from '@/components/head';
 
 export const metadata: Metadata = myMetadata;
 
@@ -15,17 +16,7 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="cs">
-      <head>
-        <meta charSet="UTF-8" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-      </head>
+      <Head />
       <body
         className={clsx(
           inter.className,
