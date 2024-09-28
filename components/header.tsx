5cdd7967601dbbf1472f68354a0eef1efaa286d6
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-09-28 16:04:25 +0200

    refactor: Oddělen input z Header do samostatné komponenty CityInput - Přesunut input a logika načítání měst z Header do nové komponenty CityInput v components/city-input.tsx - CityInput nyní obsahuje vlastní logiku pro načítání a loading - Zlepšena struktura kódu, čitelnost a modularita

--- components/header.tsx
+++ components/header.tsx
@@ -1,21 +1,7 @@
-'use client';
-
-import { useEffect } from 'react';
-import { fetchAllCities } from '@/store/thunks/fetch-all-cities';
-import { useAppSelector, useAppDispatch } from '@/store/hooks';
 import clsx from 'clsx';
+import CityInput from './city-input';
 
 export default function Header() {
-  const dispatch = useAppDispatch();
-  const allCities = useAppSelector((state) => state.city.allCities);
-  const loading = useAppSelector((state) => state.city.loading);
-
-  useEffect(() => {
-    if (!allCities.length) {
-      dispatch(fetchAllCities());
-    }
-  }, [dispatch]);
-
   return (
     <header className="bg-blue-700 bg-opacity-80 py-4 text-white shadow-md">
       <div className="container flex items-center justify-between">
@@ -27,19 +13,7 @@ export default function Header() {
         >
           G-meteo
         </h1>
-        <div className="relative w-48">
-          <input
-            type="text"
-            placeholder={loading ? 'Načítám města...' : 'Zadejte město...'}
-            disabled={loading}
-            className={clsx(
-              loading && 'cursor-not-allowed opacity-50',
-              'w-full rounded-lg bg-blue-500 bg-opacity-50 p-2 text-white placeholder-white transition',
-              'duration-200 focus:bg-opacity-100 focus:shadow-lg focus:outline-none focus:ring-4',
-              'text-center text-sm focus:ring-blue-400'
-            )}
-          />
-        </div>
+        <CityInput />
       </div>
     </header>
   );
