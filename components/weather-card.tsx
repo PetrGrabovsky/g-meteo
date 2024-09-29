bcdcf7d6c6441424b0989f1c376e9586f26bbb51
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-09-29 23:06:04 +0200

    feat: Do WeatherCard přidán textový popis stavu počasí na základě ikony - Vylepšena HTML struktura a přístupnost pomocí aria tagů v komponentě WeatherCard - Vylepšena responzivita komponenty WeatherCard

--- components/weather-card.tsx
+++ components/weather-card.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 import Image from 'next/image';
 import React, { useMemo } from 'react';
 import { isToday } from '@/utils/helpers';
+import { getDescriptionFromIcon } from '@/utils/helpers';
 
 interface WeatherCardProps {
   record: WeatherDayRecord;
@@ -16,11 +17,16 @@ const WeatherCard: React.FC<WeatherCardProps> = React.memo(({ record, isActive, 
   const weekday = useMemo(() => date.toLocaleDateString('cs-CZ', { weekday: 'long' }), [date]);
   const fullDate = useMemo(() => date.toLocaleDateString('cs-CZ'), [date]);
   const isDateToday = useMemo(() => isToday(date), [date]);
+  const translatedDescription = useMemo(() => getDescriptionFromIcon(record.icon), [record.icon]);
 
   return (
     <article
       onMouseOver={onMouseEnter}
       onMouseLeave={onMouseLeave}
+      aria-labelledby={`weather-${record.date}`}
+      role="region"
+      tabIndex={0}
+      aria-label={`Předpověď na ${fullDate}, ${translatedDescription}, teplota ${record.temp} °C, vlhkost ${record.humidity}%`}
       className={clsx(
         isActive ? 'scale-105 transform bg-opacity-30 shadow-lg' : 'hover:scale-105 hover:bg-opacity-30',
         isDateToday && 'text-blue-700',
@@ -28,20 +34,38 @@ const WeatherCard: React.FC<WeatherCardProps> = React.memo(({ record, isActive, 
         'duration-200'
       )}
     >
-      <div className="flex items-center justify-between">
-        <h3 className="text-2xl font-semibold">{weekday}</h3>
-        <p className="text-lg">{fullDate}</p>
-      </div>
+      <header className="flex items-center justify-between">
+        <h3 id={`weather-${record.date}`} className="text-2xl font-semibold">
+          {weekday}
+        </h3>
+        <time className="text-lg" dateTime={record.date}>
+          {fullDate}
+        </time>
+      </header>
       <hr className="my-3 border-t border-white border-opacity-20" />
-      <div className="mb-4 flex items-center justify-between">
-        <p className="mb-1 text-4xl font-bold">{record.temp}°C</p>
-        <Image
-          src={`https://openweathermap.org/img/wn/${record.icon}@2x.png`}
-          alt="Ikona počasí"
-          width={100}
-          height={100}
-          className="h-24 w-24"
-        />
+      <div className="mb-4 flex items-center justify-between sm:flex-col sm:justify-center">
+        <p className="mb-1 text-4xl font-bold" aria-live="polite">
+          {record.temp}°C
+        </p>
+        <div className="flex flex-col items-center justify-center">
+          <Image
+            src={`https://openweathermap.org/img/wn/${record.icon}@2x.png`}
+            alt={`Ikona počasí: ${translatedDescription}`}
+            width={100}
+            height={100}
+            className="h-24 w-24"
+          />
+          <p
+            aria-label={`Popis počasí: ${translatedDescription}`}
+            role="img"
+            className={clsx(
+              'mt-2 rounded-full border-2 border-blue-200 bg-blue-400 px-3 py-1 text-center text-sm font-semibold',
+              'text-white shadow-lg'
+            )}
+          >
+            {translatedDescription}
+          </p>
+        </div>
       </div>
       <div className="space-y-2 text-lg">
         <p>Vlhkost: {record.humidity}%</p>
