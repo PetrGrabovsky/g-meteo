a1d0dede7530dc10c45065c8736266bfb31843cb
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-09-29 11:03:43 +0200

    fix: Upravena přístupnost a SEO v komponentě Header - Přidán skrytý h1 element - Logo označeno jako aria-hidden

--- components/header.tsx
+++ components/header.tsx
@@ -6,17 +6,17 @@ export default function Header() {
   return (
     <header className="bg-blue-700 bg-opacity-80 py-4 text-white shadow-md">
       <div className="container flex items-center justify-between">
-        <h1
-          aria-label="G-Meteo - Předpověď počasí"
+        <h1 className="sr-only">Gmeteo - Předpověď počasí</h1>
+        <div
+          aria-hidden="true"
           className={clsx(
             'text-2xl font-bold uppercase tracking-wide transition-transform duration-200 hover:scale-110',
             'flex cursor-default items-center space-x-1 hover:text-blue-300'
           )}
         >
-          <span className="sr-only">G-Meteo</span>
           <Brand className="h-10 w-10" primaryColor="text-white" secondaryColor="text-green-500" />
           <span>Meteo</span>
-        </h1>
+        </div>
         <CityInput />
       </div>
     </header>
