3d4b027588deb2efa4ead3721b4a9a32f45dd851
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-10-01 15:48:47 +0200

    style: Definována animace fadeInDown v tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,9 +20,18 @@ const config: Config = {
           '0%': { opacity: '0' },
           '100%': { opacity: '1' },
         },
+        fadeInDown: {
+          '0%': {
+            transform: 'translateY(-50px)',
+          },
+          '100%': {
+            transform: 'translateY(0)',
+          },
+        },
       },
       animation: {
         fadeIn: 'fadeIn 0.5s ease-in-out',
+        fadeInDown: 'fadeInDown 0.5s ease-out',
       },
     },
   },
