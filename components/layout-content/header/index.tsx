ff9abdf67a2be1725fece0548b3b55b0239e2e8c
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-10-01 12:56:11 +0200

    refactor: Ve složce layout-content vytvořena nová složka header - Komponenta Header a Brand přesunuty do složky header - Soubor s Header přejmenován na index.tsx

--- components/layout-content/header/index.tsx
+++ components/layout-content/header/index.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import CityAutocomplete from '../city-autocomplete';
+import CityAutocomplete from '@/components/city-autocomplete';
 import Brand from './brand';
 import Link from 'next/link';
 import { useAppDispatch } from '@/store/hooks';
