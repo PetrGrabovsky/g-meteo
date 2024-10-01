f27760cd1e0a8811c6fb7a5cccddcbcae9aecb86
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-10-01 16:17:13 +0200

    refactor: Optimalizováno zpracování placeholderu a přidáno sledování chyby do CityInput - Přidána funkce getPlaceholder pro přehlednější zpracování textu placeholderu - Pokud dojde k chybě při zpracování měst zobrazí se místo placeholderu křížek a input bude disabled - Podmíněn autofocus na input pouze na nemobilních zařízeních pomocí stavu isMobile

--- components/layout/header/city-autocomplete/city-input.tsx
+++ components/layout/header/city-autocomplete/city-input.tsx
@@ -8,14 +8,17 @@ export default function CityInput() {
   const dispatch = useAppDispatch();
   const searchInput = useAppSelector((state) => state.city.searchInput);
   const cityInputRef = useRef<HTMLInputElement>(null);
+  const cityError = useAppSelector((state) => state.city.error);
 
   const cityLoading = useAppSelector((state) => state.city.loading);
   const weatherLoading = useAppSelector((state) => state.weather.loading);
   const isLoading = cityLoading || weatherLoading;
 
+  const isMobile = useAppSelector((state) => state.ui.isMobile);
+
   useEffect(() => {
-    if (!isLoading && cityInputRef.current) cityInputRef.current.focus();
-  }, [isLoading]);
+    if (!isMobile && !isLoading && cityInputRef.current) cityInputRef.current.focus();
+  }, [isLoading, isMobile]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(setSearchInput(e.target.value));
@@ -27,6 +30,13 @@ export default function CityInput() {
     dispatch(setCitySuggestions([]));
   };
 
+  const getPlaceholder = (): string => {
+    if (cityError) return '❌';
+    if (cityLoading) return 'Načítám města...';
+    if (weatherLoading) return 'Načítám počasí...';
+    return 'Zadejte město...';
+  };
+
   return (
     <input
       ref={cityInputRef}
@@ -34,14 +44,14 @@ export default function CityInput() {
       value={searchInput}
       onChange={handleInputChange}
       onBlur={handleInputBlur}
-      placeholder={cityLoading ? 'Načítám města...' : weatherLoading ? 'Načítám počasí...' : 'Zadejte město...'}
-      disabled={isLoading}
+      placeholder={getPlaceholder()}
+      disabled={isLoading || !!cityError}
       maxLength={20}
       className={clsx(
-        isLoading && 'cursor-not-allowed opacity-50',
         'w-full rounded-lg bg-blue-500 bg-opacity-50 p-2 text-white placeholder-white transition',
         'duration-200 focus:bg-opacity-100 focus:shadow-lg focus:outline-none focus:ring-4',
-        'animate-fadeIn text-center text-sm hover:scale-105 focus:scale-105 focus:ring-blue-400'
+        'animate-fadeIn text-center text-sm hover:scale-105 focus:scale-105 focus:ring-blue-400',
+        'disabled:cursor-not-allowed disabled:opacity-50'
       )}
     />
   );
