2934e820ab0834ac0a832851a7cd90f889360447
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-10-01 13:00:16 +0200

    refactor: Přesun složky city-autocomplete do složky layout-content/header

--- components/layout-content/header/index.tsx
+++ components/layout-content/header/index.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import CityAutocomplete from '@/components/city-autocomplete';
+import CityAutocomplete from '@/components/layout-content/header/city-autocomplete';
 import Brand from './brand';
 import Link from 'next/link';
 import { useAppDispatch } from '@/store/hooks';
